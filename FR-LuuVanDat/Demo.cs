48b7dbf97c620c099443de7ee4b699bd7b5534ca
Your Name <you@example.com>
2021-12-31 14:45:37 +0700

    first

---
+++ FR-LuuVanDat/Demo.cs
@@ -0,0 +1,22 @@
+﻿using HelloWord;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FR_LuuVanDat
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    class Demo
+    {
+        public static void Main(string[] args)
+        {
+            Program.sayHi();
+        }
+
+    }
+
+}
---
+++ HelloWord/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWord
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+
+        }
+        public static void sayHi()
+        {
+            string name;
+            Console.WriteLine("Enter your name: ");
+            name = Console.ReadLine();
+            Console.WriteLine("Hello {0}", name);
+            Console.WriteLine("Welcome to NTQ!!!");
+        }
+
+    }
+}
